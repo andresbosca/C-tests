97c8ef991395112a75f142a8ac87ad6714524ad0
andresbosca <88239983+andresbosca@users.noreply.github.com>
2022-09-23 15:38:52 -0300

    Adiciona reader do header de mp3

---
+++ mp3Reader.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct mp3Tag
+{
+    char tag[3];
+    char title[30];
+    char artist[30];
+    char album[30];
+    char year[4];
+    char comment[30];
+    int genre;
+};
+
+int fileNameLength = 1024;
+int mp3TagSize = 128;
+
+int main()
+{
+    mp3Tag *tagStruct = malloc(sizeof(mp3Tag));
+    char fileName[fileNameLength + 1];
+    FILE *mp3File;
+    char buffer[mp3TagSize + 1];
+
+    strcpy(fileName, "x.mp3");
+
+    mp3File = fopen(fileName, "rb");
+    buffer[mp3TagSize] = '\0'; // just a precaution
+
+    if (mp3File)
+    {
+        printf("Yeah, %s has been opened \n", fileName);
+    }
+    else
+    {
+        printf("***Aborting*** Oops... Something went wrong opening %s \n", fileName);
+        return 1;
+    }
+
+    // move file pointer to 128 bytes off the end of the file.
+    fseek(mp3File, -(mp3TagSize), SEEK_END);
+    if (mp3File == NULL)
+    {
+        printf("Could not seek, Aborting \n");
+        return 1;
+    }
+
+    printf("Starting to get the data from de file... \n");
+
+    // get the data
+    if (fgets(buffer, mp3TagSize, mp3File) != NULL)
+    {
+        tagStruct = (struct mp3Tag *)buffer;
+        printf("\nTag: %3s\n", tagStruct->tag);
+        printf("Title: %30s\n", tagStruct->title);
+        printf("Artist: %30s\n", tagStruct->artist);
+        printf("Album: %30s\n", tagStruct->album);
+        printf("Year: %4s\n", tagStruct->year);
+        printf("Comment: %30s\n", tagStruct->comment);
+        printf("Genre: %d\n", tagStruct->genre);
+
+        printf("Successful read of %s \n", fileName);
+    }
+    else
+    {
+        printf("Error reading the ID3V1 tag.");
+        return 1;
+    }
+
+    fclose(mp3File);
+    return 0;
+}
